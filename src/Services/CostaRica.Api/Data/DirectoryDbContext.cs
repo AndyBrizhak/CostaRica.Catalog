a2db6bdd83763367a784537bdc3a53d1d83d0f15
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2026-03-03 20:20:32 +0800

    feat(api): add entity sets to DirectoryDbContext

---
+++ src/Services/CostaRica.Api/Data/DirectoryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+
+namespace CostaRica.Api.Data;
+
+public class DirectoryDbContext : DbContext
+{
+    public DirectoryDbContext(DbContextOptions<DirectoryDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Здесь будут свойства DbSet
+    public DbSet<Province> Provinces => Set<Province>();
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<TagGroup> TagGroups => Set<TagGroup>();
+    public DbSet<Tag> Tags => Set<Tag>();
+    public DbSet<GoogleCategory> GoogleCategories => Set<GoogleCategory>();
+    public DbSet<MediaAsset> MediaAssets => Set<MediaAsset>();
+    public DbSet<BusinessPage> BusinessPages => Set<BusinessPage>();
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Здесь будет конфигурация Fluent API
+    }
+}
