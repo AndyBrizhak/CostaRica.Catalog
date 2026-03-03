78798b9dec841b57360c4f232dea4becd6da434b
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2026-03-03 14:52:13 +0800

    feat(api): add improved GoogleCategory with GCID and l10n

---
+++ src/Services/CostaRica.Api/Data/GoogleCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace CostaRica.Api.Data;
+
+public class GoogleCategory
+{
+    public Guid Id { get; set; }
+
+    // Google Category ID (GCID) с pleper.com, например: "abortion_clinic" или "restaurant"
+    public string Gcid { get; set; } = string.Empty;
+
+    // Название на английском (Name En)
+    public string NameEn { get; set; } = string.Empty;
+
+    // Название на испанском (Name Es)
+    public string NameEs { get; set; } = string.Empty;
+}
