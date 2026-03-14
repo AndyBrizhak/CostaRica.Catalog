df731ff0792ab6b80cc2a44340acebc8cc4b4c2b
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2026-03-14 12:18:38 +0800

    feat: add IGoogleCategoryService interface

---
+++ src/Backend/CostaRica.Api/Services/IGoogleCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using CostaRica.Api.DTOs;
+
+namespace CostaRica.Api.Services;
+
+public interface IGoogleCategoryService
+{
+    Task<IEnumerable<GoogleCategoryResponseDto>> GetAllAsync();
+    Task<GoogleCategoryResponseDto?> GetByIdAsync(Guid id);
+    Task<GoogleCategoryResponseDto?> GetByGcidAsync(string gcid);
+
+    // Создание (вернет null, если Gcid уже существует)
+    Task<GoogleCategoryResponseDto?> CreateAsync(GoogleCategoryUpsertDto dto);
+
+    Task<bool> UpdateAsync(Guid id, GoogleCategoryUpsertDto dto);
+    Task<bool> DeleteAsync(Guid id);
+}
